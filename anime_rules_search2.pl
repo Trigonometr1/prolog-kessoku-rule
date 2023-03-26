eec9c88b6efac7ed272f572d2420c2a3713f4806
Andre Septiano <andreseptiano169@gmail.com>
2023-03-26 12:04:52 +0700

    Handle cases when the year is null

--- anime_rules_search2.pl
+++ anime_rules_search2.pl
@@ -22,18 +22,21 @@ cast_year_season(Season,Year,Result) :-
     Result is Year * 10 + SeasonInt.
 
 airing_before(Season,Year,Name) :-
+    Year \== null,
     anime(Name,_,_,_,_,SeasonA,YearA,_,_),
     cast_year_season(SeasonA,YearA,ResA),
     cast_year_season(Season,Year,Res),
     ResA < Res.
 
 airing_after(Season,Year,Name) :-
+    Year \== null,
     anime(Name,_,_,_,_,SeasonA,YearA,_,_),
     cast_year_season(SeasonA,YearA,ResA),
     cast_year_season(Season,Year,Res),
     ResA > Res.
 
 airing_at(Season,Year,Name) :-
+    Year \== null,
     anime(Name,_,_,_,_,SeasonA,YearA,_,_),
     cast_year_season(SeasonA,YearA,ResA),
     cast_year_season(Season,Year,Res),
